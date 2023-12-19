5502287e29e884031a54817c6ee25026a38b91c2
efoppiano <efoppiano@fi.uba.ar>
2023-12-19 17:18:39 -0300

    Disable StrictHostKeyChecking in initialize.jl

--- src/initialize.jl
+++ src/initialize.jl
@@ -4,5 +4,5 @@ begin
     ips_folder = readdir("ips")
     ips = map(file -> readlines("ips/$file")[1], ips_folder)
 
-    addprocs(ips)
+    addprocs(ips; sshflags=["-o StrictHostKeyChecking=no", "-o UserKnownHostsFile=/dev/null"])
 end
