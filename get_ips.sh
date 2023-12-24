6729a30da2f9aa0f9ec3b1648a70ef46a201efd0
efoppiano <efoppiano@fi.uba.ar>
2023-12-24 04:33:44 -0300

    Minor improvement to get_ips.sh

--- get_ips.sh
+++ get_ips.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-apt-get update
-apt-get install -y dnsutils
-
 counter=1
 for ip in $(nslookup tasks.worker | awk '/^Address:/ && NR > 2 {print $2}'); do
   echo "$ip" > "/ips/ip_${counter}"
